dfd7cd70ffcdfc9aeb27ec6b66115b4b4448a18b
Shayabro <shaiabro@gmail.com>
2022-03-12 21:57:00 +0200

    First Shayabro commit

--- experiments/dir.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.append("C:/Users/ShayAbramovich/WSB_Bot")
-print(os.getcwd())
-import config
-print(config.DB_HOST)
-
--- experiments/try.py
+++
@@ -1,7 +0,0 @@
-from psaw import PushshiftAPI
-
-
-api=PushshiftAPI()
-sub = list(api.search_comments(subreddit='wallstreetbets',limit=25))
-for s in sub:
-    print(s)
